b35322a592959f35fac962fccd971e0b683a63a4
贾利鹏 <33855833+TibetanWolfAnd233@users.noreply.github.com>
2025-03-17 13:28:39 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+// Overwrite Hello World. 
+int main()
+{
+    // Binary Search. 
+    bsearch(main, main, 0, 0, main);
+
+    // Quick Sort. 
+    qsort(main, 0, 0, main);
+
+    return 0;
+}
