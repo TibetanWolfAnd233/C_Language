4ba1b637c0ec3973acfd3ac7218621b8aa23c3e8
贾利鹏 <33855833+TibetanWolfAnd233@users.noreply.github.com>
2025-03-17 13:34:20 +0800

    Create niam.c

---
+++ invert/niam.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+// Overwrite Hello World. 
+int main()
+{
+    // Quick Sort. 
+    qsort(main, 0, 0, main);
+
+    // Binary Search. 
+    bsearch(main, main, 0, 0, main);
+
+    return 0;
+}
